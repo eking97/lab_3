07e861ce4d6e76bd2c86bbea6770b49b9f135768
eking97 <emki0195@colorado.edu>
2020-02-13 20:43:17 -0700

    Updated file

--- lab3_script.sh
+++ lab3_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Authors : Emma King and Megan McGinnis
-# Date: 2/7/2020
+# Date: 2/6/20
 
 #Problem 1 Code:
 #Make sure to document how you are solving each problem!
@@ -8,15 +8,8 @@ echo "Enter a filename: "
 read fileName
 echo "Enter a regular expression: "
 read regExp
-grep "$regExp" "$fileName"
-
-echo "Number of lines that contain phone numbers: "
-grep -c "[0-9][0-9][0-9]-" "regex_practice.txt"
-
-echo "Count of emails: "
-grep -c ".com" "regex_practice.txt"
-
-echo "List of all phone numbers in 303 area code: "
-grep "^303-" "regex_practice.txt"
-
-grep "@geocities.com" "regex_practice.txt" >> email_results.txt
+egrep $regExp $fileName
+egrep -c "^[0-9][0-9][0-9]-" $fileName
+egrep -c "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$" $fileName
+egrep -c "^303-" $fileName
+grep "@geocities.com" $fileName >> email_results.txt
